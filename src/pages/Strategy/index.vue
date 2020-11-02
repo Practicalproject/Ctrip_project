07e8d1773d19bc17b1dc98326262f7f16defaf9f
18830974630 <691577405@qq.com>
2020-11-02 17:41:03 +0800

    黄跟新了

---
+++ src/pages/Strategy/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Strategy</div>
+</template>
+
+<script>
+export default {
+  name: 'Strategy',
+}
+</script>
+
+<style lang='less' scoped>
+
+</style>
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,26 @@
 import AirlinePage from '@/pages/AirlinePage'
 import TrainPage from '@/pages/TrainPage'
+import Tickets from '@/pages/Tickets'
+import Strategy from '@/pages/Strategy'
 
 export default [
     {
-        path:'/airlinepage',
-        component:AirlinePage
+        path: '/airlinepage',
+        component: AirlinePage
     },
     {
-        path:'/trainPage',
-        component:TrainPage
+        path: '/trainPage',
+        component: TrainPage
     },
+    {
+        // 门票
+        path: '/tickets',
+        component: Tickets
+    },
+    {
+        // 攻略
+        path: '/strategy',
+        component: Strategy
+    },
+
 ]
