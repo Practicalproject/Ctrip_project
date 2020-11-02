28f32e0c3cf05d9557faeb58cd155abe85630e5c
baohua <852622779@qq.com>
2020-11-02 16:00:00 +0800

    bh路由

---
+++ src/pages/IndependentTravel/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="root">
+    <div class="imvc-view-item"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndependentTravel",
+};
+</script>
+
+<style lang="less" scoped>
+#root {
+  width: 1180px;
+  height: 200px;
+  background-color: pink;
+  margin: 0 auto;
+}
+</style>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,19 @@
 import AirlinePage from '@/pages/AirlinePage'
 import TrainPage from '@/pages/TrainPage'
+import IndependentTravel from '@/pages/IndependentTravel'
+
 
 export default [
     {
-        path:'/airlinepage',
-        component:AirlinePage
+        path: '/airlinepage',
+        component: AirlinePage
     },
     {
-        path:'/trainPage',
-        component:TrainPage
+        path: '/trainPage',
+        component: TrainPage
+    },
+    {
+        path: '/independenttravel',
+        component: IndependentTravel
     },
 ]
