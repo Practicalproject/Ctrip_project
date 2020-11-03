9a486211563bdb81bcac565aa1970317d94cd164
baohua <852622779@qq.com>
2020-11-03 18:01:49 +0800

    bh页面

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import { Carousel } from 'element-ui';
+import { Carousel, CarouselItem } from 'element-ui';
 Vue.use(Carousel);
+Vue.use(CarouselItem);
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 
