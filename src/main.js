7b00a76bd49a4bd9a0591b52c6243d9538217d76
18830974630 <691577405@qq.com>
2020-11-03 10:29:05 +0800

    飞更新了

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-import { Carousel } from 'element-ui';
-Vue.use(Carousel);
+import { Carousel, CarouselItem, Card } from 'element-ui';
+
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
-
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Card);
 new Vue({
   render: h => h(App),
   router
