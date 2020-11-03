4f422e4560572b3e23f5f3253e684c88749ffa0b
吕鹏超“git config --global user.name 吕鹏超“ <772340536@qq.com>
2020-11-03 21:38:34 +0800

    主页局部组件

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,13 +1,18 @@
 <template>
-  <div></div>
+  <div>
+    <CarRental></CarRental>
+  </div>
 </template>
 
 <script>
+import CarRental from "CarRental";
 export default {
-  name: 'Home',
-}
+  name: "Home",
+  components: {
+    CarRental,
+  },
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
