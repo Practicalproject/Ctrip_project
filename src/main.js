58472dce3661841c09ee4f509baf6865632397cd
18830974630 <691577405@qq.com>
2020-11-03 16:37:22 +0800

    飞修改了

--- src/main.js
+++ src/main.js
@@ -1,20 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
-<<<<<<< HEAD
-import { Carousel,Card,Option } from 'element-ui';
-Vue.use(Carousel);
-Vue.use(Card);
-Vue.use(Option);
-=======
-import { Carousel, CarouselItem, Card } from 'element-ui';
->>>>>>> d73d8c289dc8a284676feafe9ff7705f8eb8e04f
+import { Carousel, Card, Option, CarouselItem } from 'element-ui';
+
 
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
 Vue.use(Carousel);
 Vue.use(CarouselItem);
 Vue.use(Card);
+Vue.use(Option);
 new Vue({
   render: h => h(App),
   router
