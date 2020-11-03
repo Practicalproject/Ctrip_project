4bfcfe974542f0a99db32b6fae08f75c1fc23c8b
suolabi <suolabi@163.com>
2020-11-03 21:29:38 +0800

    'suoxiaoyu'

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div></div>
+  <div>
+    <Partner></Partner>
+    <Footer></Footer>
+  </div>
 </template>
 
 <script>
+import Partner from './Partner'
+import Footer from './Footer'
 export default {
   name: 'Home',
+  components:{
+    Partner,
+    Footer
+  }
 }
 </script>
 
