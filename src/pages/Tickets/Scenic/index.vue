f86404c5db2629fe3d4110d62b5e5224073d77c2
18830974630 <691577405@qq.com>
2020-11-04 16:07:37 +0800

    飞更新了

--- src/pages/Tickets/Scenic/index.vue
+++ src/pages/Tickets/Scenic/index.vue
@@ -11,14 +11,17 @@
     </div>
     <!-- 中间主体 -->
     <div class="contain_middle">
-      <div class="middle_item">
+      <el-card class="middle_item">
         <!-- 左侧图片 -->
         <div class="item_image">
           <img src="./images/01.jpg" alt="" />
         </div>
         <!-- 右侧文本 -->
-        <div class="item_title"></div>
-      </div>
+        <div class="item_title">
+          <div class="title_left">123</div>
+          <div class="title_right">asda</div>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
@@ -33,7 +36,7 @@ export default {
 .scenic_contain {
   width: 1180px;
   margin: 20px auto;
-  background-color: yellow;
+  // background-color: yellow;
   // 头部区域
   .contain_header {
     display: flex;
@@ -55,15 +58,32 @@ export default {
     .middle_item {
       width: 800px;
       height: 150px;
-      background-color: yellowgreen;
       margin: 0 20px 10px 0;
-      .item_image {
-        width: 243px;
-        height: 150px;
-        background-color: red;
-        img {
-          width: 100%;
-          height: 100%;
+      border-radius: 5px;
+      /deep/.el-card__body {
+        padding: 0;
+        display: flex;
+        .item_image {
+          width: 243px;
+          height: 150px;
+          background-color: red;
+          img {
+            width: 100%;
+            height: 100%;
+            border-radius: 5px 0 0 5px;
+          }
+        }
+        .item_title {
+          width: 200px;
+          height: 200px;
+          background-color: red;
+          display: flex;
+          .title_left{
+
+          }
+          .title_right{
+            
+          }
         }
       }
     }
