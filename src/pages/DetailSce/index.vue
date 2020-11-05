9572c11507cfb08b9f711b4975f833628bb337c3
baohua <852622779@qq.com>
2020-11-05 11:49:58 +0800

    bhDetail

--- src/pages/DetailSce/index.vue
+++ src/pages/DetailSce/index.vue
@@ -1,13 +1,24 @@
 <template>
   <!-- 经典详情 -->
-  <div>hello kitty</div>
+  <div>
+    <DetailBan></DetailBan>
+  </div>
 </template>
 
 <script>
+import DetailBan from "./DetailBan";
 export default {
+  components: {
+    DetailBan,
+  },
   name: "DetailSce",
 };
 </script>
 
 <style lang="less" scoped>
+div {
+  // width: 100%;
+  // height: 100%;
+  // background: #f2f5f7;
+}
 </style>
